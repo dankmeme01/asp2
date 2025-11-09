0933f5f9ebfb34d439d1d0e94f4222846a3a47ff
dankmeme01 <42031238+dankmeme01@users.noreply.github.com>
2025-11-09 18:56:22 +0100

    aaa

--- include/asp/format.hpp
+++ include/asp/format.hpp
@@ -20,4 +20,18 @@ std::string_view local_format(std::array<char, N>& arr, fmt::format_string<Args.
     return std::string_view{arr.data(), size};
 }
 
+template <size_t N, typename... Args>
+std::string_view local_format(char(&arr)[N], fmt::format_string<Args...> fmt, Args&&... args) {
+    auto size = fmt::format_to_n(&arr[0], N, fmt, std::forward<Args>(args)...).size;
+
+    if (size < N) {
+        arr[size] = '\0';
+    } else {
+        arr[N - 1] = '\0';
+    }
+
+    return std::string_view{&arr[0], size};
+}
+
+
 }
