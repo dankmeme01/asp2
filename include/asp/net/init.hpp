3faaa61fbe78f6d4bbf9f2648ad45385fc42b290
dankmeme01 <42031238+dankmeme01@users.noreply.github.com>
2026-01-23 22:55:26 +0100

    remove networking, now obsolete due to qsox

--- include/asp/net/init.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-namespace asp::net {
-    // Initialize networking libraries.
-    // Note: this is not recommended to be called manually, as it is called automatically by the library.
-    void init();
-    void cleanup();
-}
